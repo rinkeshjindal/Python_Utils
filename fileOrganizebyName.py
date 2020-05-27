c59dd087f2233a0b42019f3201f6830a8a25f0ce
rinkeshjindal <rinkeshjindal@gmail.com>
2020-05-27 05:37:15 -0700

    Update fileOrganizebyName.py

--- fileOrganizebyName.py
+++ fileOrganizebyName.py
@@ -16,7 +16,7 @@ DIRECTORIES = {
     "DOCUMENTS": ["oxps", "epub", "pages", "docx", "doc", "fdf", "ods", "csv",
                 "odt", "pwi", "xsn", "xps", "dotx", "docm", "dox", 
                 "rvg", "rtf", "rtfd", "wpd", "xls", "xlsm", "xlsx", "xltx", "ppt",
-                "pptx"], 
+                "pptx","prn"],
     "ARCHIVES": ["a", "ar", "cpio", "iso", "tar", "gz", "rz", "7z", 
                 "dmg", "rar", "xar", "zip"], 
     "AUDIO": ["aac", "aa", "aac", "dvf", "m4a", "m4b", "m4p", "mp3", 
@@ -26,7 +26,8 @@ DIRECTORIES = {
     "PYTHON": ["py"], 
     "XML": ["xml"], 
     "EXE": ["exe"], 
-    "SHELL": ["sh"] 
+    "SHELL": ["sh"],
+    "Tax": ["T14","T15","T16","T17","T18","T19","T20"]
 } 
 
 FILE_FORMATS = {file_format: directory 
@@ -39,9 +40,10 @@ folder = {'Visa': ['visa','797'],
           'Alisha': ['Alisha', 'jiya'],
           'Pooja': ['Pooja', 'rimpy'],
           'Sharvil': ['Sharvil'],
-          'resume': ['resume', 'profile', 'it_prof'],
+          'resume': ['resume', 'it_prof'],
           'education': ['education', 'MCA', 'Graduation'],
           'finance': ['tax', 'statement_', 'savings', 'mortgage'],
+          'salary': ['salary', 'payslip','paycheck']
           }
 
 def search(values, file_name):
@@ -60,57 +62,68 @@ def organize_files(scr_folder, target_folder):
             cat_folder = search(folder, name)
             if cat_folder != None:
                 src_dir = os.path.join(root, name)
+                p = Path(target_folder)
+                if target_folder.rpartition('\\')[2] != cat_folder:  # check folder if already exists
+                    destination_folder = Path.joinpath(p, cat_folder)
+                    destination_folder.mkdir(exist_ok=True)
+                    print("Created folder: "+str(destination_folder))
+                else:
+                    destination_folder = target_folder
+                #p = Path(destination_folder)
                 try:
-                    shutil.move(src_dir, target_folder+"/"+cat_folder+"/"+name, copy_function='copy2')
+                    shutil.move(src_dir, str(destination_folder)+"/"+name, copy_function='copy2')
                     print("moved: " + name)
                 except:
-                    print("unable to move dir: "+ name)
+                    print("unable to move dir: "+ src_dir + " to " +target_folder+"/"+cat_folder+"/"+name)
+                    #raise
                     pass
 
+        #print("root: " + root)
         for each_file in files:
             cat_folder = search(folder, each_file)
             if cat_folder != None:
-                print("file: "+cat_folder)
                 src_file = os.path.join(root, each_file)
                 file_format = each_file.split(".")[-1].lower()
                 file_name = each_file.split(".")[0]
-
-                create_time = os.path.getmtime(src_file)
-                str_create_time = time.strftime("%Y%b%d", gmtime(create_time)) #Create Date Format
-
-                if (str_create_time == file_name.rpartition('_')[2]):    #add date to filename if not already
-                    new_file_name = each_file
-                else:
-                    new_file_name = file_name +"_" + str_create_time +"."+file_format
-
-                p = Path(target_folder)
-                if target_folder.rpartition('\\')[2] != cat_folder: #check folder if already exists
-                    destination_folder = Path.joinpath(p, cat_folder)
-                    destination_folder.mkdir(exist_ok=True)
-                else:
-                    destination_folder = target_folder
-                p = Path(destination_folder)
+                #print("file: " + each_file)
                 if file_format in FILE_FORMATS:
-                    f = FILE_FORMATS[file_format]
-                    cur_dir = target_folder.rpartition('\\')[2]
-                    if cur_dir != f: #check folder if already exists
-                        destination_folder = Path.joinpath(p, f)
+                    create_time = os.path.getmtime(src_file)
+                    str_create_time = time.strftime("%Y%b%d", gmtime(create_time)) #Create Date Format
+
+                    if (str_create_time == file_name.rpartition('_')[2]):    #add date to filename if not already
+                        new_file_name = each_file
+                    else:
+                        new_file_name = file_name +"_" + str_create_time +"."+file_format
+                    #print("target_folder: " + str(target_folder))
+                    p = Path(target_folder)
+                    if target_folder.rpartition('\\')[2] != cat_folder: #check category folder if already exists
+                        destination_folder = Path.joinpath(p, cat_folder)
                         destination_folder.mkdir(exist_ok=True)
                     else:
                         destination_folder = target_folder
+                    #print("destination_folder: "+str(file_format))
+                    p = Path(str(destination_folder))
 
+                    f = FILE_FORMATS[file_format]
+                    cur_dir = str(destination_folder).rpartition('\\')[2]
+                    if cur_dir != f: #check fileFormat folder if already exists
+                        destination_folder = Path.joinpath(p, f)
+                        destination_folder.mkdir(exist_ok=True)
+                    else:
+                        destination_folder = destination_folder
+                    #print("before Final_folder: " + str(destination_folder))
                     final_path = str(destination_folder) + "\\" + new_file_name
-
+                    #print("final_path: "+str(final_path))
                     if src_file != final_path: #move / rename files
-                            if Path(final_path).exists():
-                                base_filename = new_file_name.split(".")[0]
-                                ctr = 1
-                                while(Path(final_path).exists()):
-                                        new_file_name = base_filename + "_copy" + str(ctr)+ "." + file_format
-                                        final_path = str(destination_folder) + "\\" + new_file_name
-                                        ctr = ctr + 1
-                            shutil.move(src_file, final_path, copy_function='copy2')
-                            print('moved file')
+                        if Path(final_path).exists():
+                            base_filename = new_file_name.split(".")[0]
+                            ctr = 1
+                            while(Path(final_path).exists()):
+                                new_file_name = base_filename + "_copy" + str(ctr)+ "." + file_format
+                                final_path = str(destination_folder) + "\\" + new_file_name
+                                ctr = ctr + 1
+                        shutil.move(src_file, final_path, copy_function='copy2')
+                        print('moved file' + src_file +" to "+ final_path)
 
 if __name__ == "__main__":
     if len(sys.argv) != 3:
